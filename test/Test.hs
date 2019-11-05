ffe1b0d0ca025d669da820ca80f05e9f6c28d723
Maria Kuklina <mkdirwtf@gmail.com>
2019-11-05 17:08:35 +0300

    Upgrade testing

--- test/Test.hs
+++ test/Test.hs
@@ -26,64 +26,84 @@ import qualified DTResidualize as DTR
 
 import qualified LogicInt as LI
 import qualified List as L
+import qualified Sort as L
+import qualified Num as L
+import qualified Programs as L
 
---
--- Save a tree into pdf file.
---
-printToPdf :: DotPrinter a => String -> a -> IO ExitCode
-printToPdf name t = do
-    let dotfilename = name ++ ".dot"
-    let pdffilename = name ++ ".pdf"
-    printTree dotfilename t
-    system $ "dot -Tpdf '" ++ dotfilename ++ "' > '" ++ pdffilename ++ "'"
-
---
--- Open pdf file of a tree (using `zathura` pdf viewer).
---
-openInPdf :: DotPrinter a => a -> IO ExitCode
-openInPdf t = do
-    let name = "/tmp/ukanrentesttree"
-    openInPdfWithName name t
-
-openInPdfWithName :: DotPrinter a => String -> a -> IO ExitCode
-openInPdfWithName name t = do
-    let dotfilename = name ++ ".dot"
-    let pdffilename = name ++ ".pdf"
-    printTree dotfilename t
-    -- dot -Tpdf '$dot' > '$tmp_pdf_file'
-    system $ "dot -Tpdf '" ++ dotfilename ++ "' > '" ++ pdffilename ++ "'"
-    -- zathura '$pdf'
-    system $ "zathura '" ++ pdffilename ++ "'"
-    -- rm '$pdf' '$dot'
-    system $ "rm '" ++ pdffilename ++ "' '" ++ dotfilename ++ "'"
-
-ocanren filename goal = do
-  let p = pur goal
-  let name = filename ++ ".ml"
-  OC.topLevel name "topLevelSU" Nothing p
-  where
-    pur goal = let
-        (tree, logicGoal, names) = SU.topLevel goal
-        f = DTR.topLevel tree
-        (goal', names', defs) = P.purification (f, vident <$> reverse names)
-      in (goal', names', (\(n1, n2, n3) -> (n1, n2, fromJust $ DTR.simplify n3)) <$> defs)
-
-ocanrenCPD filename goal = do
-  let (t, lg, n) = GC.topLevel goal
-  let f = CR.residualizationTopLevel t
-  let p = P.purification (f, vident <$> reverse n)
-  let name = printf "%s.ml" filename
-  OC.topLevel name "topLevelCPD" Nothing p
+import TestUtils
 
 open g = openInPdf $ fst3 $ SU.topLevel g
 
-openMarked g = openInPdf $ DTR.cutFailedDerivations $ DTR.makeMarkedTree $ fst3 $ SU.topLevel g
+cutTree = DTR.cutFailedDerivations . DTR.makeMarkedTree . fst3 . SU.topLevel
+openCut = openInPdf . cutTree
 
-testRev2 = L.reverso $ fresh ["xs", "sx"] $
+
+testRevRev = L.reverso $ fresh ["xs", "sx"] $
   call "reverso" [V "xs", V "sx"] &&& call "reverso" [V "sx", V "xs"]
 
-testRev2Res = L.reverso $
+testRevRevRes = L.reverso $
   call "reverso" [list, listR] &&& call "reverso" [listR, list]
   where
     list = C "x" [] L.% C "y" [] L.% C "z" [] L.% L.nil
     listR = C "z" [] L.% C "y" [] L.% C "x" [] L.% L.nil
+
+testMaxLen = L.maxLengtho $ fresh ["x", "m", "l"] $
+  call "maxLengtho" [x, m, l]
+  where
+    x = V "x"
+    m = V "m"
+    l = V "l"
+
+-- Test double appendo
+testDA = L.doubleAppendo $ fresh ["a", "b", "c", "d"]
+              (call "doubleAppendo" [V "a", V "b", V "c", V "d"])
+
+
+-- Test double appendo
+testDA1 = L.doubleAppendo $ fresh ["a", "b", "c", "d"]
+              (call "doubleAppendo" [V "a", V "a", V "c", V "d"])
+
+
+-- Test double appendo
+testDA2 = L.doubleAppendo $ fresh ["a", "b", "c"]
+              (call "doubleAppendo" [V "a", V "b", V "c", V "c"])
+
+
+testDA3 = L.doubleAppendo $ fresh ["a", "b", "c", "d"]
+              (call "doubleAppendo" [V "d", V "b", V "d", V "c"])
+
+-- Test reverse without acc
+testRev = L.reverso $ fresh ["a", "b"]
+              (call "reverso" [V "a", V "b"])
+
+-- Test reverse without acc'
+testRev2 = L.reverso $ fresh ["a", "b"]
+              (call "reverso" [L.peanify 1 L.% L.peanify 2 L.% L.peanify 3 L.% L.nil, V "b"])
+
+-- Test reverse with acc
+testRevac = L.revAcco $ fresh ["a", "b", "acc"]
+              (call "revacco" [V "a", V "acc", V "b"])
+
+-- Test reverse with acc (nil acc)
+testRevac2 = L.revAcco $ fresh ["a", "b"]
+              (call "revacco" [V "a", L.nil, V "b"])
+
+testMaxo = L.maxo $ fresh ["a", "r"]
+           (call "maxo" [V "a", V "r"])
+
+testSort = L.sorto $ fresh ["xs", "ys"]
+           (call "sorto" [V "xs", V "ys"])
+
+testCall = outter $
+  fresh ["x", "y", "z", "i"] $ call "outter" [V "x", V "y", V "z", V "i"]
+
+outter :: G a -> G a
+outter g =
+  Let (def "outter" ["x", "y", "z", "i"] (call "g" [V "x", V "y"] &&& call "f" [V "z", V "i"])) $ gfun $ ffun g
+
+gfun :: G a -> G a
+gfun g =
+  Let (def "g" ["x", "y"] (V "x" === L.nil ||| (call "listo" [V "y"]))) $ L.listo g
+
+ffun :: G a -> G a
+ffun g = Let (def "f" ["x", "y"] (call "listo" [V "x"])) $ L.listo g
