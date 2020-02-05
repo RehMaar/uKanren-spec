5190463c9e170c70392fe836d05aeb947ea80f43
Maria Kuklina <mkdirwtf@gmail.com>
2020-02-05 14:35:53 +0300

    Add new unfold method; need fix a bug of resid

---
+++ src/Unfold/UnrecUnfold.hs
@@ -0,0 +1,81 @@
+module Unfold.UnrecUnfold where
+    
+import Syntax
+import DTree
+
+import qualified CPD
+import qualified Eval as E
+import qualified Purification as P
+import qualified GlobalControl as GC
+
+import Utils
+
+import Data.Function (on)
+import Data.Maybe (mapMaybe)
+import Data.List
+import qualified Data.Set as Set
+
+import Text.Printf
+import DotPrinter
+import Unfold.Unfold
+
+import Debug.Trace
+import Control.Exception (assert)
+
+trace' _ = id
+
+data UUGoal = UUGoal DGoal deriving Show
+
+topLevel :: G X -> (DTree, G S, [S])
+topLevel g = let
+  (lgoal, lgamma, lnames) = goalXtoGoalS g
+  lgoal' = CPD.normalize lgoal
+  igoal = assert (length lgoal' == 1) $ UUGoal (head lgoal')
+  tree = fst3 $ derivationStep igoal Set.empty lgamma E.s0 Set.empty 0
+  in (tree, lgoal, lnames)
+
+instance Unfold UUGoal where
+  getGoal (UUGoal dgoal) = dgoal
+
+  initGoal goal = UUGoal goal
+
+  emptyGoal (UUGoal dgoal) = null dgoal
+
+  mapGoal (UUGoal dgoal) f = UUGoal (f dgoal)
+
+  unfoldStep = unreqUnfoldStep
+
+unreqUnfoldStep :: UUGoal -> E.Gamma -> E.Sigma -> ([(E.Sigma, UUGoal)], E.Gamma)
+unreqUnfoldStep (UUGoal dgoal) env subst = let
+    (conj, rest) = splitGoal env dgoal
+    (newEnv, uConj) = unfold conj env
+
+    nConj = goalToDNF uConj
+    unConj = unifyAll subst nConj
+    us = (\(cs, subst) -> (subst, suGoal subst cs rest)) <$> unConj
+  in (us, newEnv)
+  where
+    suGoal subst cs rest = UUGoal $ E.substituteConjs subst $ cs ++ rest
+
+splitGoal :: E.Gamma -> DGoal -> (G S, [G S])
+splitGoal env gs = let (c:cs) = sortBy ((compare `on` (isRec env)) <>
+                                        compareGoals env) gs
+                    in (c, cs)
+
+isRec :: E.Gamma -> G S -> Bool
+isRec (p, _, _) goal@(Invoke call _) =
+  let (name, args, body) = p call in
+  any ((== name) . getInvokeName) $ getInvokes body
+  where
+    getInvokes b = concat $ filter isInvoke <$> normalize b
+isRec _ _ = False
+
+compareGoals :: E.Gamma -> G a -> G a -> Ordering
+compareGoals (p, _, _) (Invoke g1 _) (Invoke g2 _)
+  | g1 == g2
+  = EQ
+  | otherwise
+  = let n1 = length $ normalize $ trd3 $ p g1
+        n2 = length $ normalize $ trd3 $ p g2
+    in compare n1 n2
+compareGoals _ _ _ = EQ
